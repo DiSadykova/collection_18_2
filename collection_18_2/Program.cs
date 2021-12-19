fdf15755e7611b2a5e6b6df02724845c0d0b6b92
Диляра Садыкова <dilyara.mozhet.sadykova@gmail.com>
2021-12-19 12:09:05 +0300

    Добавьте файлы проекта.

---
+++ collection_18_2/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace collection_18_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string startString = "({()}([{}])[])[]";
+            char[] array = startString.ToCharArray();
+            List<char> charList = new List<char>();
+            charList.AddRange(array);
+
+            Stack<char> que = new Stack<char>();
+
+            for (int i = 0; i < charList.Count; i++)
+            {
+
+                switch (charList[i])
+                {
+                    case '(':
+                        que.Push(')');
+                        break;
+                    case '{':
+                        que.Push('}');
+                        break;
+                    case '[':
+                        que.Push(']');
+                        break;
+                }
+                char chEx;
+                switch (charList[i])
+                {
+                    case ')':
+                        chEx=que.Pop();
+                        if (chEx ==')')
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Скобки расставлены не корректно");
+                            Console.ReadKey();
+                            return;
+                        }
+                    case '}':
+                        chEx = que.Pop();
+                        if (chEx == '}')
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Скобки расставлены не корректно");
+                            Console.ReadKey();
+                            return;
+                        }
+                    case ']':
+                        chEx = que.Pop();
+                        if (chEx == ']')
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Скобки расставлены не корректно");
+                            Console.ReadKey();
+                            return;
+                        }
+                        
+                }
+            }
+            if (que.Count==0)
+            {
+                Console.WriteLine("Скобки расставлены корректно");
+            }
+            else
+            {
+                Console.WriteLine("Скобки расставлены не корректно");
+            }    
+            Console.ReadKey();
+        }
+    }
+}
